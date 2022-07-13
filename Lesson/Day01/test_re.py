f52f6fcd2767daec7dbc0fa135ab42fb7ddd1c0d
YuChuanxing <chuanxing.yu@cienet.com>
2022-07-13 10:07:08 +0800

    learn python

---
+++ Lesson/Day01/test_re.py
@@ -0,0 +1,14 @@
+# Author: Fishycx
+# Time: 2022/7/8 9:55 AM
+
+import re
+
+# re.match 只匹配字符串的开始，如果字符串开始不符合正则表达式，则匹配失败，函数返回 None，而 re.search 匹配整个字符串，直到找到一个匹配。
+
+
+
+# re.sub(pattern, repl, string, count=0, flags=0), Python 的re模块提供了re.sub用于替换字符串中的匹配项。
+
+#
+
+
